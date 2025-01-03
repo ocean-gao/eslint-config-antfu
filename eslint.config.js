b51e4d381f8d5cb4cc91871a65b8660dfb4e6aab
gaokeke <gaopandyx@163.com>
2025-01-03 18:38:04 +0800

    docs: 增加备注

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import { tsImport } from 'tsx/esm/api';
 const local = await tsImport('./src/index.ts', import.meta.url).then(r => r.default);
 
 export default antfu({
+  // Type of the project. 'lib' for libraries, the default is 'app'
   type: 'lib',
   ...local,
 });
